48234c37b5f49463448b10575e36325d6be38c89
xoksam <xoksamarek@gmail.com>
2021-08-06 19:29:29 +0200

    Initial project setup

---
+++ src/main/java/com/cleevio/task/eshop/EshopApplication.java
@@ -0,0 +1,13 @@
+package com.cleevio.task.eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshopApplication.class, args);
+	}
+
+}
